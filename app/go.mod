c8d79158c8fee7faed4689b9e2a4fbfab146ded5
banana <bananaic1025@gmail.com>
2025-01-11 10:09:10 +0900

    new sum go

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module chosig-launcher
+
+go 1.23.0
+
+require fyne.io/fyne/v2 v2.5.3 // indirect
--- app/main.go
+++ app/main.go
@@ -1,9 +1,29 @@
 package main
 
-import(
-	"fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	fmt.Println("hello world")
+	// 애플리케이션 생성
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Hello Fyne")
+
+	// 위젯 생성
+	label := widget.NewLabel("Hello, World!")
+	button := widget.NewButton("Click Me", func() {
+		label.SetText("Button Clicked!")
+	})
+
+	// 컨테이너 설정
+	myWindow.SetContent(container.NewVBox(
+		label,
+		button,
+	))
+
+	// 윈도우 크기와 실행
+	myWindow.Resize(fyne.NewSize(300, 200))
+	myWindow.ShowAndRun()
 }
